22f805abac581c3098e4d88213313a40d40a9bc1
Swizec Teller <swizec@swizec.com>
2019-01-30 21:10:00 -0800

    Add reakit default theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { Provider } from 'reakit'
+import theme from 'reakit-theme-default'
 
 import Header from './header'
 import './layout.css'
@@ -17,23 +19,25 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with ❤️ by{` `}
-            <a href="https://www.swizec.com">Swizec</a>
-          </footer>
-        </div>
-      </>
+      <Provider theme={theme}>
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            {children}
+            <footer>
+              © {new Date().getFullYear()}, Built with ❤️ by{` `}
+              <a href="https://www.swizec.com">Swizec</a>
+            </footer>
+          </div>
+        </>
+      </Provider>
     )}
   />
 )
