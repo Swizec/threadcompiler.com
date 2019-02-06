667fca5c1999d970f6a1fada10ab9ec237c1ccb8
Swizec Teller <swizec@swizec.com>
2019-02-05 22:44:38 -0800

    Showing proper errors

--- src/components/TweetButton.js
+++ src/components/TweetButton.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
 import { Button } from 'reakit'
 import { post } from 'httpie'
+import TweetEmbed from 'react-tweet-embed'
 
 import Auth, { PRIVATE_ENDPOINT } from '../utils/auth'
 import { ThreadContainer } from '../state'
@@ -8,32 +9,49 @@ import { ThreadContainer } from '../state'
 const auth = new Auth()
 
 export default () => {
-  const [message, setMessage] = useState('')
-  const { input } = useContext(ThreadContainer.Context)
   const { isAuthenticated } = auth
+  const [success, setSuccess] = useState(null)
+  const [error, setError] = useState('')
+  const [tweet, setTweet] = useState({})
+  const { input } = useContext(ThreadContainer.Context)
 
   const callPrivate = async () => {
     const token = auth.getToken()
 
-    const res = await post(PRIVATE_ENDPOINT, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: {
-        user_id: auth.getUser().sub,
-        message: input,
-      },
-    })
+    try {
+      const res = await post(PRIVATE_ENDPOINT, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: {
+          user_id: auth.getUser().sub,
+          message: input,
+        },
+      })
+      const { data, statusCode } = res
 
-    const { data, statusCode } = res
-
-    console.log(statusCode, data.message)
-    setMessage(JSON.stringify(data.message))
+      if (statusCode === 200) {
+        setSuccess(true)
+        setTweet(data.message)
+      } else {
+        setSuccess(false)
+        setError(JSON.stringify(data))
+      }
+    } catch (err) {
+      setSuccess(false)
+      setError(JSON.stringify(err))
+    }
   }
 
   return (
     <>
-      <p>{message}</p>
+      {success ? (
+        <div>
+          <p>Great success! Your tweet looks like this 👇</p>
+          <TweetEmbed id={tweet.id_str} />
+        </div>
+      ) : null}
+      {error ? <p>Tweeting failed! {error}</p> : null}
       {isAuthenticated() && (
         <Button onClick={callPrivate}>Tweet your Thread</Button>
       )}
